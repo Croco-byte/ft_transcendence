a32f9de4274afc304573d398890b5a606bc01b1d
Yassine <Yassine@Yassine.local>
2021-07-02 17:42:30 +0200

    docker compose with postgresql nestjs/typeorm phppgadmin

---
+++ website/src/users/users.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('User')
+export class User
+{
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  avatar: string;
+
+  @Column({ type: "text"})
+  stat: string;
+
+  @Column({ type: "integer" })
+  score: number
+
+  @Column({ type: "boolean", default: false })
+  DFA: boolean
+
+  @Column({type: "boolean", default: true})
+  online: boolean
+};
