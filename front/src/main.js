d808263b6b1e553f8f67cfd40620a9f4583c3e79
qroland <quentin.roland@sciencespo.fr>
2021-09-06 14:16:40 +0200

    cleaned old files to prepare to merge with master

--- front/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-const app = createApp(App).use(store).use(router);
-app.mount('#app');
